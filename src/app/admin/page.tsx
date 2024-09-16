1e6033614b144f621b5296ce65b84e61820cf805
Cinque Howells <cinque.howells@gmail.com>
2024-09-16 15:24:27 +1000

    added login/logout functionality and protected pages

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,16 @@
+import { SignOut } from "@/components/admin/sign-out";
+import { auth } from "@/../auth";
 
-export default function Dashboard() {
-    return (
-      "random dashboard page"
-    );
+export default async function Dashboard() {
+
+  const session = await auth();
+  if (!session) return <div>Not authenticated</div>
+ 
+  return (
+    <div>
+      <h1>Dashboard Home Page</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <SignOut/>
+    </div>
+  );
   }
---
+++ src/app/admin/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInButton from "../../../components/admin/sign-in"
+
+export default function Home() {
+    return (
+        <div>
+        <SignInButton/>
+        </div>
+    )
+}
