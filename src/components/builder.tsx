a1fbc37825539c1de507e35b12c0b3264c6b0276
Cindy <shixs2@student.unimelb.edu.au>
2024-09-24 21:29:06 +1000

    fix: refactor header and footer and make them into sections
    Co-authored-by: Noel Abraham <ncabraham@student.unimelb.edu.au>

--- src/components/builder.tsx
+++ src/components/builder.tsx
@@ -4,11 +4,7 @@ import { BuilderComponent, useIsPreviewing } from "@builder.io/react";
 import { BuilderContent, builder } from "@builder.io/sdk";
 import DefaultErrorPage from "next/error";
 import "../builder-registry";
-import '../styles/global.css';
-
-import Header from "./layout/header";
-import Footer from "./layout/footer";
-
+import "../styles/global.css";
 
 type BuilderPageProps = ComponentProps<typeof BuilderComponent>;
 
@@ -23,13 +19,15 @@ export function RenderBuilderContent({ content, model }: BuilderPageProps) {
   // render the BuilderComponent with the specified content and model props.
   // TODO: this is a temp fix add css styles here
   if (content || isPreviewing) {
-    return <>
-          <Header/>
-          <div className="pageContainer">
+    return (
+      <>
+        <BuilderComponent model="header"/>
+        <div className="pageContainer">
           <BuilderComponent content={content} model={model} />
-          </div>
-          <Footer/>
-    </>;
+        </div>
+        <BuilderComponent model="footer"/>
+      </>
+    );
   }
   // If the "content" is falsy and the page is
   // not being previewed in Builder, render the
