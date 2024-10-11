b390c76d9cbb17569708e97e3e8422bbb50dbe57
Cinque Howells <cinque.howells@gmail.com>
2024-10-11 15:51:15 +1100

    feat: [IT-150] added additional authentication general function

--- src/app/admin/member/page.tsx
+++ src/app/admin/member/page.tsx
@@ -4,29 +4,30 @@ import MemberService from "../../../backend/service/member-service";
 import { TableWrapper } from "../../../components/admin/background";
 import AdminHeader from "@/components/admin/admin-header";
 import { MemberDisplayObjectMapper } from "@/backend/mapper/display-object-mapper";
+import { authenticate } from "@/components/admin/auth";
 
 
 export default async function Dashboard() {
-
-    const session = await auth();
-    if (!session) return <div>Not authenticated</div>
-
-    const service = new MemberService();
-    const data = await service.getAll();
-
-    const mapper = new MemberDisplayObjectMapper();
-
-    const formattedData = mapper.mapToMany(data);
-
-    return (
-      <AdminHeader>
-          <TableWrapper title={'Memberships'}>
-              <TableComponent 
-                data={formattedData.objects} 
-                columns={formattedData.columns} 
-                descriptor={'Community Members'}
-                linkedUrl={'member'}/>
-          </TableWrapper>
-      </AdminHeader>
+    return authenticate(
+      async () => {
+        const service = new MemberService();
+        const data = await service.getAll();
+    
+        const mapper = new MemberDisplayObjectMapper();
+    
+        const formattedData = mapper.mapToMany(data);
+    
+        return (
+          <AdminHeader>
+              <TableWrapper title={'Memberships'}>
+                  <TableComponent 
+                    data={formattedData.objects} 
+                    columns={formattedData.columns} 
+                    descriptor={'Community Members'}
+                    linkedUrl={'member'}/>
+              </TableWrapper>
+          </AdminHeader>
+        );
+      }
     );
 }
