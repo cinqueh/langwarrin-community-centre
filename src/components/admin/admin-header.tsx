c6ea4ee2aaf792500d3aa3f735bcf0ff9bb12c24
Cinque Howells <115082768+cinqueh@users.noreply.github.com>
2024-10-12 15:48:00 +1100

    feat: [IT-252] Linked up admin header (#58)

--- src/components/admin/admin-header.tsx
+++ src/components/admin/admin-header.tsx
@@ -1,42 +1,23 @@
 import { ReactNode } from "react";
-import { Anton } from 'next/font/google';
 import styles from './shared.module.css';
 import SignOut from "./sign-out";
+import NavigationLinks from "./navigation";
 
 type AdminHeaderProps = {
     children: ReactNode;
 };
 
-const anton = Anton({
-    weight: '400',
-    subsets: ['latin'],
-});
-
-const AdminHeader: React.FC<AdminHeaderProps> = ({
-    children
-}) => {
+const AdminHeader = ({ children }: AdminHeaderProps) => {
     return (
-        <>
-            <header className={styles.header}>
-                <nav className={styles.nav}>
-                    <a href="#" className={`${styles.navItem} ${styles.active}`}>
-                        Members
-                    </a>
-                    <a href="#" className={styles.navItem}>
-                        Program Enrolments
-                    </a>
-                    <div className={`${styles.adminPortal} ${anton.className}`}>
-                        Admin Portal
-                        <span className={styles.centerName}>Langwarrin Community Centre</span>
-                    </div>
-                    <a href="#" className={styles.navItem}>
-                        Room Bookings
-                    </a>
-                    <SignOut/>
-                </nav>
-            </header>
-            {children}
-        </>
+      <>
+        <header className={styles.header}>
+          <nav className={styles.nav}>
+            <NavigationLinks />
+            <SignOut />
+          </nav>
+        </header>
+        {children}
+      </>
     );
 };
 
