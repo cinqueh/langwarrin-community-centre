04755fbcf742d470b4aaecb2ffb688d9b70e1e1d
Cindy <shixs2@student.unimelb.edu.au>
2024-10-23 21:23:06 +1100

    make childcare form style consistant, connect childcare form to backend, make the form moblie respomsive

--- src/components/childcare-form/childcare-form.tsx
+++ src/components/childcare-form/childcare-form.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React, { useState } from "react";
 import styles from "./styles.module.css";
+import { ChildDTO } from "@/backend/dto/childcare/child";
+import { PersonDTO } from "@/backend/dto/person";
+import { ChildcareInquiryDTO } from "@/backend/dto/inquiry";
 
-type ChildcareContactFormProps = {
+interface ChildcareContactFormProps {
   title: string;
   subtitle: string;
   firstNamePlaceholder: string;
@@ -15,24 +18,9 @@ type ChildcareContactFormProps = {
   childLastNamePlaceholder: string;
   programPlaceholder: string;
   messagePlaceholder: string;
-  submitButtonText: string;
-};
+}
 
-const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
-  title,
-  subtitle,
-  firstNamePlaceholder,
-  lastNamePlaceholder,
-  emailPlaceholder,
-  mobilePlaceholder,
-  homePhonePlaceholder,
-  occupationPlaceholder,
-  childFirstNamePlaceholder,
-  childLastNamePlaceholder,
-  programPlaceholder,
-  messagePlaceholder,
-  submitButtonText,
-}) => {
+const ChildcareContactForm = (props: ChildcareContactFormProps) => {
   const [formData, setFormData] = useState({
     title: "",
     firstName: "",
@@ -45,8 +33,13 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
     childLastName: "",
     childAge: "",
     program: "",
+    selectedDays: [] as string[],
     message: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
   const handleInputChange = (
     e: React.ChangeEvent<
@@ -57,18 +50,84 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    setFormData((prevData) => {
+      const selectedDays = checked
+        ? [...prevData.selectedDays, value]
+        : prevData.selectedDays.filter((day) => day !== value);
+      return { ...prevData, selectedDays };
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Handle form submission logic
+
+    const childDTO = new ChildDTO({
+      childAge: parseInt(formData.childAge),
+      childFirstName: formData.childFirstName,
+      childSurname: formData.childLastName,
+    });
+
+    const personDTO = new PersonDTO({
+      personId: 0,
+      firstName: formData.firstName,
+      surname: formData.lastName,
+      email: formData.email,
+      phoneNumber: formData.mobile,
+      homeNumber: formData.homePhone,
+      occupation: formData.occupation,
+    });
+
+    const selectedDaysString = formData.selectedDays.join(" ");
+
+    const childcareInquiryDTO = new ChildcareInquiryDTO({
+      date: new Date(),
+      person: personDTO,
+      child: childDTO,
+      day: selectedDaysString,
+      program: formData.program,
+      notes: formData.message,
+    });
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("/api/enquiry/childcare", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(childcareInquiryDTO),
+      });
+
+      if (response.ok) {
+        setAlertMessage("Form submitted successfully!");
+        setAlertType("success");
+      } else {
+        setAlertMessage(
+          "An error occurred while submitting the form. Please try again."
+        );
+        setAlertType("error");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setAlertMessage(
+        "An error occurred while submitting the form. Please try again."
+      );
+      setAlertType("error");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <div className={styles.childcareContactForm}>
-      <h4>{title}</h4>
-      <p className={styles.subtitle}>{subtitle}</p>
+    <div className={styles.formContainer}>
+      <h3 className={styles.title}>{props.title}</h3>
+      <p className={styles.subtitle}>{props.subtitle}</p>
       <form onSubmit={handleSubmit}>
-        <h5>Parent Contact Information</h5>
-        <div className={styles.inputGroup}>
+        <h5 className={styles.sectionTitle}>Parent Contact Information</h5>
+        <div className={styles.nameInputGroup}>
           <select
             name="title"
             value={formData.title}
@@ -85,7 +144,7 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="text"
             name="firstName"
-            placeholder={firstNamePlaceholder}
+            placeholder={props.firstNamePlaceholder}
             value={formData.firstName}
             onChange={handleInputChange}
             required
@@ -93,7 +152,7 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="text"
             name="lastName"
-            placeholder={lastNamePlaceholder}
+            placeholder={props.lastNamePlaceholder}
             value={formData.lastName}
             onChange={handleInputChange}
             required
@@ -101,12 +160,12 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
         </div>
 
         <div className={styles.inputGroup}>
-          <div className={styles.mobileInputGroup}>
-            <span className={styles.mobilePrefix}>+61</span>
+          <div className={styles.mobileInputWrapper}>
+            <span className={styles.mobilePrefix}>+61 |</span>
             <input
-              type="text"
+              type="tel"
               name="mobile"
-              placeholder={mobilePlaceholder}
+              placeholder={props.mobilePlaceholder}
               value={formData.mobile}
               onChange={handleInputChange}
               required
@@ -115,7 +174,7 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="text"
             name="homePhone"
-            placeholder={homePhonePlaceholder}
+            placeholder={props.homePhonePlaceholder}
             value={formData.homePhone}
             onChange={handleInputChange}
             required
@@ -126,7 +185,7 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="email"
             name="email"
-            placeholder={emailPlaceholder}
+            placeholder={props.emailPlaceholder}
             value={formData.email}
             onChange={handleInputChange}
             required
@@ -134,19 +193,19 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="text"
             name="occupation"
-            placeholder={occupationPlaceholder}
+            placeholder={props.occupationPlaceholder}
             value={formData.occupation}
             onChange={handleInputChange}
             required
           />
         </div>
 
-        <h5>Child&#39;s Information</h5>
-        <div className={styles.inputGroup}>
+        <h5 className={styles.sectionTitle}>Child's Information</h5>
+        <div className={styles.nameInputGroup}>
           <input
             type="text"
             name="childAge"
-            placeholder="Child's Age"
+            placeholder="Age"
             value={formData.childAge}
             onChange={handleInputChange}
             required
@@ -154,7 +213,7 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="text"
             name="childFirstName"
-            placeholder={childFirstNamePlaceholder}
+            placeholder={props.childFirstNamePlaceholder}
             value={formData.childFirstName}
             onChange={handleInputChange}
             required
@@ -162,14 +221,14 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
           <input
             type="text"
             name="childLastName"
-            placeholder={childLastNamePlaceholder}
+            placeholder={props.childLastNamePlaceholder}
             value={formData.childLastName}
             onChange={handleInputChange}
             required
           />
         </div>
 
-        <div className={styles.inputGroup}>
+        <div className={styles.fullLineInput}>
           <select
             name="program"
             value={formData.program}
@@ -177,23 +236,50 @@ const ChildcareContactForm: React.FC<ChildcareContactFormProps> = ({
             className={styles.selectInput}
             required
           >
-            <option value="">{programPlaceholder}</option>
+            <option value="">{props.programPlaceholder}</option>
             <option value="Koala Group">Koala Group</option>
             <option value="Possum Group">Possum Group</option>
           </select>
         </div>
 
+        <p>Select Days</p>
+        <div className={styles.checkBoxGroup}>
+          {days.map((day) => (
+            <label key={day} className={styles.dayLabel}>
+              <input
+                type="checkbox"
+                value={day}
+                checked={formData.selectedDays.includes(day)}
+                onChange={handleCheckboxChange}
+              />
+              {day}
+            </label>
+          ))}
+        </div>
+
         <textarea
           name="message"
-          placeholder={messagePlaceholder}
+          placeholder={props.messagePlaceholder}
           value={formData.message}
           onChange={handleInputChange}
           className={styles.messageTextarea}
-          required
         />
 
-        <button type="submit" className="button-white">
-          {submitButtonText}
+        {alertMessage && (
+          <p
+            className={alertType === "success" ? "alertSuccess" : "alertError"}
+          >
+            {alertMessage}
+          </p>
+        )}
+
+        <button
+          type="submit"
+          className="button-white"
+          style={{ display: "block", margin: "0 auto" }}
+          disabled={isLoading}
+        >
+          {isLoading ? "Submitting..." : "Submit"}
         </button>
       </form>
     </div>
